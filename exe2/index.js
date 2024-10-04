4de8d3efb2816ee26d98a1db72a9d5ac8b542efa
MhamdiAbdelilah <abdelilah.mhamdi@lycee-jeanrostand.fr>
2024-10-04 19:54:33 +0200

    Q-3.1

--- exe2/index.js
+++ exe2/index.js
@@ -5,6 +5,7 @@
 cord = { lat: 48.960488945948185, lng: 2.5478907124310757 }
 
 async function initMap() {
+  const { Map, InfoWindow } = await google.maps.importLibrary("maps");
   fetch('markersJrostand.json')
   .then((res) => {
     if (!res.ok) {
@@ -13,14 +14,14 @@ async function initMap() {
     return res.json();
   })
   .then((markers) => {
-    const { Map, InfoWindow } = google.maps.importLibrary("maps");
     const map = new google.maps.Map(document.getElementById("map"), {
       zoom: 15,
       center: cord,
       mapId: "37467c5a4504d37e",
     });
+    const infoWindow = new InfoWindow();
 
-    setMarkers(map, markers);
+    setMarkers(map, markers, infoWindow);
   })
   .catch((error) => {
     console.error('Error fetching markers:', error);
@@ -52,32 +53,44 @@ async function initMap() {
 
 // Data for the markers consisting of a name, a LatLng and a zIndex for the
 // order in which these markers should display on top of each other.
-async function setMarkers(map, markers) {
-  const { AdvancedMarkerElement, PinElement } = await google.maps.importLibrary(
+async function setMarkers(map, markers, infoWindow) {
+  const  { AdvancedMarkerElement, PinElement } = await google.maps.importLibrary(
     "marker",
   );
   
       
-      for (let i = 0; i < markers.length; i++) {
-        const beach = markers[i];
+  for (let i = 0; i < markers.length; i++) {
+    const beach = markers[i];
 
-        const marker = new AdvancedMarkerElement({
-          position: beach,
-          map,
-      });
-    
-    
-    }
-    
-    const flightPath = new google.maps.Polyline({
-      path: markers,
-      geodesic: true,
-      strokeColor: "#FF0000",
-      strokeOpacity: 1.0,
-      strokeWeight: 2,
+    const pin = new PinElement({
+      glyph: `${i + 1}`,
+      scale: 1,
     });
-  
-    flightPath.setMap(map);
+    const marker = new AdvancedMarkerElement({
+      position: beach,
+      map,
+      title: `${i+1}`,
+      content: pin.element,
+      gmpClickable: true,
+
+
+  });
+  marker.addListener("click", ({ domEvent, latLng }) => {
+    const { target } = domEvent;
+    alert(`Vous avez clicer sur le marker ${marker.title}`)
+  });
+
+
+}
+  const flightPath = new google.maps.Polyline({
+    path: markers,
+    geodesic: true,
+    strokeColor: "#FF0000",
+    strokeOpacity: 1.0,
+    strokeWeight: 2,
+  });
+
+  flightPath.setMap(map);
 
 }
 
